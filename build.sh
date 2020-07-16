0cf16bcb275a13d54faa189f6447237a5705bd22
noeppi_noeppi <noeppinoeppi@gmail.com>
2020-07-16 11:23:17 +0200

    Include compiled javascript files in the tarballs

--- build.sh
+++ build.sh
@@ -4,26 +4,32 @@ CWD=`pwd`
 
 cd ../nodecg-io
 
+echo "Call \`npm install\`"
+npm install
+
+echo "Call \`npm run build\`"
+npm run build
+
 ls -d nodecg-io-* | while read z; do
   echo "Create tarball for ${z}"
   cd ${z}
   if [[ ${z} == 'nodecg-io-core' ]]; then
     cp package.json package.json---tarball
     python3 ${CWD}/replace_json.py package.json
-    tar --exclude 'node_modules' --exclude '*.js' --exclude 'package.json---tarball' --exclude 'dashboard' -czf ${CWD}/${z}.tar.gz .
+    tar --exclude 'node_modules' --exclude 'package.json---tarball' --exclude 'dashboard' -czf ${CWD}/${z}.tar.gz .
     mv package.json---tarball package.json
 
     cd dashboard
     echo "Create tarball for nodecg-io-dashboard"
     cp package.json package.json---tarball
     python3 ${CWD}/replace_json.py package.json
-    tar --exclude 'node_modules' --exclude '*.js' --exclude 'package.json---tarball' -czf ${CWD}/nodecg-io-dashboard.tar.gz .
+    tar --exclude 'node_modules' --exclude 'package.json---tarball' -czf ${CWD}/nodecg-io-dashboard.tar.gz .
     mv package.json---tarball package.json
     cd ..
   else
     cp package.json package.json---tarball
     python3 ${CWD}/replace_json.py package.json
-    tar --exclude 'node_modules' --exclude '*.js' --exclude 'package.json---tarball' -czf ${CWD}/${z}.tar.gz .
+    tar --exclude 'node_modules' --exclude 'package.json---tarball' -czf ${CWD}/${z}.tar.gz .
     mv package.json---tarball package.json
   fi
   cd ..
