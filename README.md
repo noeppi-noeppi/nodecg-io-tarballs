34982599593adaf7a2912b5eff3059b9cb303ca3
noeppi_noeppi <noeppinoeppi@gmail.com>
2020-07-16 10:42:54 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tarballs to use as dependencies in npm for nodecg-io
+
+To use one of those as a dependency in npm pu the following in your `dependencies` section:
+
+```
+"nodecg-io-core": "https://raw.githubusercontent.com/noeppi-noeppi/nodecg-io-tarballs/master/nodecg-io-core.tar.gz"
+"nodecg-io-twitch": "https://raw.githubusercontent.com/noeppi-noeppi/nodecg-io-tarballs/master/nodecg-io-twitch.tar.gz"
+```
+
+I'll try to keep this as up-to-date as possible.
---
+++ replace_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+with open(sys.argv[1], mode='r') as file:
+    data = json.loads(file.read())
+
+if 'dependencies' in data:
+    for key in data['dependencies']:
+        if key.startswith('nodecg-io-'):
+            data['dependencies'][key] = f'https://raw.githubusercontent.com/noeppi-noeppi/nodecg-io-tarballs/master/{key}.tar.gz'
+
+if 'devDependencies' in data:
+    for key in data['devDependencies']:
+        if key.startswith('nodecg-io-'):
+            data['devDependencies'][key] = f'https://raw.githubusercontent.com/noeppi-noeppi/nodecg-io-tarballs/master/{key}.tar.gz'
+
+if 'nodecg' in data and 'bundleDependencies' in data['nodecg']:
+    for key in data['nodecg']['bundleDependencies']:
+        if key.startswith('nodecg-io-'):
+            data['nodecg']['bundleDependencies'][key] = f'https://raw.githubusercontent.com/noeppi-noeppi/nodecg-io-tarballs/master/{key}.tar.gz'
+
+with open(sys.argv[1], mode='w') as file:
+    file.write(json.dumps(data))
